452ec262d830b20a29eb0e6e151dd4631421511c
wstudionatali <wstudionatali@gmail.com>
2023-12-31 00:50:33 +0100

    star rating

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -8,7 +8,7 @@
       <div class="book-author mb-4 text-lg font-semibold">by {{ $book->author }}</div>
       <div class="book-rating flex items-center">
         <div class="mr-2 text-sm font-medium text-slate-700">
-          {{ number_format($book->reviews_avg_rating, 1) }}
+        <x-star-rating :rating="$book->reviews_avg_rating" />
         </div>
         <span class="book-review-count text-sm text-gray-500">
           {{ $book->reviews_count }} {{ Str::plural('review', $book->reviews_count) }}
@@ -24,9 +24,12 @@
         <li class="book-item mb-4">
           <div>
             <div class="mb-2 flex items-center justify-between">
-              <div class="font-semibold">{{ $review->rating }}</div>
+              <div class="font-semibold">
+                <x-star-rating :rating="$review->rating" />
+              </div>
               <div class="book-review-count">
-                {{ $review->created_at->format('M j, Y') }}</div>
+                {{ $review->created_at->format('M j, Y') }}
+              </div>
             </div>
             <p class="text-gray-700">{{ $review->review }}</p>
           </div>
